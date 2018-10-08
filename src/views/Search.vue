97658f6c4fbf204d0a675ce5f570ee6589308a09
Luis Moreno <l.david1929@gmail.com>
2018-10-07 19:58:01 -0500

    epic-spinner added

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -10,9 +10,9 @@
       @keyup.enter='search'
       @click:append='search'
     )
-    //- results
     v-container(fluid grid-list-md)
-      v-layout(row wrap)
+      //- results
+      v-layout(row wrap v-if='!loading')
         v-flex(
           v-for='track in tracks'
           :key='track.id'
@@ -26,28 +26,42 @@
                 v-icon(size='18') fas fa-plus
               v-btn(icon)
                 v-icon(size='18') fas fa-heart
+      //- spinner
+      v-layout(v-else justify-center)
+        atom-spinner(
+          :size='60'
+          :animation-duration='800'
+          color='#ff1d5e'
+        )
 </template>
 
 <script>
 import { HTTP } from '@/http-common.js'
 import Aplayer from 'vue-aplayer'
+import { AtomSpinner } from 'epic-spinners'
 
 export default {
   name: 'Search',
   components: {
-    Aplayer
+    Aplayer,
+    AtomSpinner
   },
   data: () => ({
     searchValue: '',
-    tracks: []
+    tracks: [],
+    loading: false
   }),
   methods: {
     search () {
+      this.tracks = []
+      this.loading = true
       let url = `music/tracks/?search=${this.searchValue}`
       HTTP.get(url).then(response => {
         console.log(response.data)
         this.tracks = this.setTracks(response.data.results)
+        this.loading = false
       }).catch(e => {
+        this.loading = false
         console.log(e.response)
       })
     },
