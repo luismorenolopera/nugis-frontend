d1d0888eeb5bd4ed9735a90ba3096cdefddaca89
Luis Moreno <l.david1929@gmail.com>
2018-10-11 19:37:28 -0500

    refactor file structure

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -5,23 +5,27 @@
     grow
   )
     v-tabs-slider
+    //- tabs ui
     v-tab(href='#tab-1') YOUTUBE
       v-icon fab fa-youtube
     v-tab(href='#tab-2') ARCHIVO LOCAL
       v-icon fas fa-file-upload
+    //- tabs views
     v-tab-item(id='tab-1')
       youtube-download
     v-tab-item(id='tab-2')
-      h1 basic upload
+      upload-local-file
 </template>
 
 <script>
-import YoutubeDownload from '@/components/YoutubeDownload.vue'
+import YoutubeDownload from '@/views/YoutubeDownload.vue'
+import UploadLocalFile from '@/views/UploadLocalFile.vue'
 
 export default {
   name: 'Upload',
   components: {
-    YoutubeDownload
+    YoutubeDownload,
+    UploadLocalFile
   }
 }
 </script>
---
+++ src/views/UploadLocalFile.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  v-container
+    v-layout(row wrap)
+      v-flex(xs12)
+        v-text-field(
+          label='Titulo'
+          counter='100'
+          required
+        )
+      v-flex(x12)
+        v-text-field(
+          label='Imagen'
+          counter
+          maxlength='200'
+          placeholder='https://ibb.co/joa3zK'
+        )
+</template>
+
+<script>
+export default {
+  name: 'UploadLocalFile'
+}
+</script>
