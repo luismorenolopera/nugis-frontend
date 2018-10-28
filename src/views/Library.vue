8332ff2ba0b08d142dadcf462b94928670d3e2d9
Luis Moreno <l.david1929@gmail.com>
2018-10-28 17:07:19 -0500

    Library view basic load

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -1,9 +1,46 @@
 <template lang="pug">
-  h1 Library
+  v-container
+    v-list
+      div(
+        v-for='playlist in playlists'
+        :key='playlist.id'
+        class='pointer'
+        @click='openPlaylist(playlist.id)'
+      )
+        v-list-tile
+          v-list-tile-action
+            v-icon fas fa-play
+          v-list-tile-content {{ playlist.name }}
+        v-divider
 </template>
 
 <script>
+import { HTTP } from '@/http-common'
+
 export default {
-  name: 'Library'
+  name: 'Library',
+  data: () => ({
+    playlists: []
+  }),
+  created: function () {
+    let url = 'music/playlists/'
+    HTTP.get(url).then(response => {
+      console.log(response)
+      this.playlists = response.data
+    }).catch(e => {
+      console.log(e.response)
+    })
+  },
+  methods: {
+    openPlaylist (id) {
+      console.log(id)
+    }
+  }
 }
 </script>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+</style>
