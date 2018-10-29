773f605730ebd3df1d2a1bf2dff1f45248f981b1
Luis Moreno <l.david1929@gmail.com>
2018-10-29 08:00:56 -0500

    add playlist added

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
   v-container
+    //- list of playlists
     v-list
       div(
         v-for='playlist in playlists'
@@ -14,6 +15,39 @@
             v-list-tile-title {{ playlist.name }}
             v-list-tile-sub-title {{playlist.tracks.length}} canciones
         v-divider
+    //-dialog add playlist
+    v-dialog(
+      v-model='dialog'
+      max-width='500px'
+    )
+      v-card
+        v-card-title(
+          class='headline grey darken-4'
+          primary-title
+        ) Nueva playlist
+        v-card-text
+          v-text-field(
+            v-model='playlist'
+            label='playlist'
+            v-validate="'required'"
+            :error-messages="errors.collect('playlist')"
+            name='playlist'
+            @keyup.enter='validate'
+          )
+        v-card-actions
+          v-spacer
+          v-btn(flat color='primary' @click.native='validate') Crear
+    //- button add playlist
+    v-btn(
+      absolute
+      fab
+      bottom
+      right
+      color="primary"
+      @click="dialog = !dialog"
+    )
+      span(class='display-1 font-weight-bold') +
+
 </template>
 
 <script>
@@ -22,7 +56,9 @@ import { HTTP } from '@/http-common'
 export default {
   name: 'Library',
   data: () => ({
-    playlists: []
+    playlists: [],
+    dialog: false,
+    playlist: ''
   }),
   created: function () {
     let url = 'music/playlists/'
@@ -36,6 +72,24 @@ export default {
     openPlaylist (playlist) {
       this.$store.commit('setTracks', { tracks: playlist.tracks })
       this.$router.push({ path: '/playlist', query: { id: playlist.id } })
+    },
+    validate () {
+      if (this.errors.items.length > 0) return
+      return this.$validator.validateAll().then((result) => {
+        if (!result) return
+        this.addPlaylist()
+      })
+    },
+    addPlaylist () {
+      this.dialog = false
+      let url = 'music/playlists/'
+      HTTP.post(url, {
+        name: this.playlist
+      }).then(response => {
+        console.log(response)
+      }).catch(e => {
+        console.log(e)
+      })
     }
   }
 }
