789397e094ad5d8c86b589997d9aff712356b5d1
Luis Moreno <l.david1929@gmail.com>
2018-10-30 09:52:19 -0500

    home view basic added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,46 @@
 <template lang='pug'>
-  div
-    h1 home
-    img(src='@/assets/logo.png' alt='nugis' class='mb-5' height='100' width='100')
+  v-container(fluid grid-list-md)
+    //- results
+    v-layout(row wrap v-if='!loading')
+      v-flex(
+        v-for='track in tracks'
+        :key='track.id'
+        xs12 sm6
+      )
+        app-track(:track='track')
+    //- loader
+    atom-spinner(
+      v-else
+      :size='150'
+      :animation-duration='800'
+      color='#ff1d5e'
+      class='centered'
+    )
 </template>
+<script>
+import { HTTP } from '@/http-common.js'
+import AppTrack from '@/components/Track.vue'
+import { AtomSpinner } from 'epic-spinners'
+
+export default {
+  name: 'Home',
+  data: () => ({
+    tracks: [],
+    loading: false
+  }),
+  components: {
+    AppTrack,
+    AtomSpinner
+  },
+  created: function () {
+    this.loading = true
+    let url = 'music/tracks/'
+    HTTP.get(url).then(response => {
+      this.tracks = response.data.results
+      this.loading = false
+    }).catch(e => {
+      this.loading = false
+    })
+  }
+}
+</script>
